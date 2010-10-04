1aeafc9c75e5835cdb05767246ae8dc179ff0c0a
sroccaserra <sroccaserra@yahoo.com>
2010-10-04 23:01:00 +0200

    Clojure craddle now uses Midje

--- languages/Clojure/project.clj
+++ languages/Clojure/project.clj
@@ -1,5 +1,6 @@
 (defproject xxxx "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [midje "0.6.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- languages/Clojure/test/xxxx_test.clj
+++ languages/Clojure/test/xxxx_test.clj
@@ -1,5 +1,6 @@
 (ns xxxx-test
     (:use clojure.test)
+    (:use midje.sweet)
     (:use xxxx :reload-all))
 
 (deftest failure
